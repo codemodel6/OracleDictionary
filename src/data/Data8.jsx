cbc2bf223410140996c2a634f7853e9e1238a55c
codemodel6 <codemodel6@gmail.com>
2023-05-30 09:27:46 +0900

    7장 완료

---
+++ src/data/Data8.jsx
@@ -0,0 +1,19 @@
+data8 = [{
+    Name : "",
+    Explanation : "",
+    Query : "",
+    Details : "",
+    EXQuery : "",
+    EXExplanation : "",
+    image : ,
+},
+{
+    Name : "",
+    Explanation : "",
+    Query : "",
+    Details : "",
+    EXQuery : "",
+    EXExplanation : "",
+    image : ,
+},
+];
